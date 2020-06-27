7f1596a64273647d8fa562ffaa16cd36bcf7c429
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-26 20:55:37 -0700

    set up local state for signup and login forms

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -2,18 +2,45 @@ import React, { useState } from "react";
 import { Row, Col, Form, Button } from "react-bootstrap";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    handle: "",
+    password: "",
+  });
+  const { handle, password } = formData;
+
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <Row>
       <Col md={{ span: 6, offset: 3 }}>
         <h1 className="display-4">Login</h1>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Label>Handle</Form.Label>
-            <Form.Control type="text" placeholder="Enter handle" />
+            <Form.Control
+              type="text"
+              placeholder="Enter handle"
+              value={handle}
+              name="handle"
+              onChange={handleChange}
+            />
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              name="password"
+              onChange={handleChange}
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
             Login
--- client/src/components/pages/Signup.js
+++ client/src/components/pages/Signup.js
@@ -1,30 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Form, Button } from "react-bootstrap";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    handle: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+  });
+  const { handle, email, password, passwordConfirm } = formData;
+
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <Row>
       <Col md={{ span: 6, offset: 3 }}>
         <h1 className="display-4">Signup</h1>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Label>Handle</Form.Label>
-            <Form.Control type="text" placeholder="Enter handle" />
+            <Form.Control
+              type="text"
+              placeholder="Enter handle"
+              onChange={handleChange}
+              name="handle"
+              value={handle}
+            />
           </Form.Group>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={handleChange}
+              name="email"
+              value={email}
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+              name="password"
+              value={password}
+            />
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Confirm Password</Form.Label>
-            <Form.Control type="password" placeholder="Confirm Password" />
+            <Form.Control
+              type="password"
+              placeholder="Confirm Password"
+              onChange={handleChange}
+              name="passwordConfirm"
+              value={passwordConfirm}
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
             Signup
